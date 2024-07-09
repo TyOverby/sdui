0784535fb0c4c2a9d9844251d7a82cb811b5e3ee
Ty Overby <ty@pre-alpha.com>
2024-07-08 22:40:42 -0400

    _

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -f index.html main.bc.js
-cp ../sdui/_build/default/bin/{index.html,main.bc.js} ./ 
+cp ../sdui2/_build/default/bin/{index.html,main.bc.js} ./ 
 git add -A
 git commit -m "_"
 git push
