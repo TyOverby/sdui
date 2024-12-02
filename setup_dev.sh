98d8c85471f02fe5cf9f039e50647a081a44d4fc
Ty Overby <ty@pre-alpha.com>
2024-12-02 15:20:09 -0500

    Update setup_dev.sh

--- setup_dev.sh
+++ setup_dev.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-sudo bash -c "sh <(curl -fsSL https://opam.ocaml.org/install.sh)"
-opam init
-opam update
+curl -fsSL https://opam.ocaml.org/install.sh > install_opam.sh
+chmod +x install_opam.sh
+./install_opam.sh --download-only
 
-opam repo add with-extensions https://github.com/janestreet/opam-repository.git#with-extensions
-opam switch create ./ 5.2.0+flambda2 --repos with-extensions,default --empty
-eval $(opam env)
+./opam init
+./opam update
 
-opam install ./ \
+./opam repo add with-extensions https://github.com/janestreet/opam-repository.git#with-extensions
+./opam switch create ./ 5.2.0+flambda2 --repos with-extensions,default --empty
+eval $(./opam env)
+
+./opam install ./ \
  ocamlformat.0.26.2+jst \
  merlin.5.2.1-502+jst \
  ocaml-lsp-server.1.19.0+jst
