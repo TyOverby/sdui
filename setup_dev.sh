8a1160fcc52aac272643ed4beb6d5da29a99b285
Ty Overby <ty@pre-alpha.com>
2024-12-02 15:15:41 -0500

    Update setup_dev.sh

--- setup_dev.sh
+++ setup_dev.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo apt install opam
+bash -c "sh <(curl -fsSL https://opam.ocaml.org/install.sh)"
 opam init
 opam update
 
 opam repo add with-extensions https://github.com/janestreet/opam-repository.git#with-extensions
-opam switch create ./ 5.2.0+flambda2 --repos with-extensions,default
+opam switch create ./ 5.2.0+flambda2 --repos with-extensions,default --empty
 eval $(opam env)
 
 opam install ./ \
