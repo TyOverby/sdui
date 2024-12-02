5c62d6f58a1b521de18239adfb974fe9f63ff88b
Ty Overby <ty@pre-alpha.com>
2024-12-02 15:56:30 -0500

    Update setup_dev.sh

--- setup_dev.sh
+++ setup_dev.sh
@@ -12,12 +12,10 @@ chmod +x opam
 rm sdui.opam
 
 ./opam init --bare
-./opam update
-
 ./opam repo add with-extensions https://github.com/janestreet/opam-repository.git#with-extensions --dont-select
+./opam update
 ./opam switch create ./ 5.2.0+flambda2 --repos with-extensions,default --no-install
 eval $(./opam env)
-./opam update
 
 ./opam install \
  ocamlformat.0.26.2+jst \
