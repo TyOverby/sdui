3869dc8f3f4899369a8d48c829172a965d3c9172
Ty Overby <ty@pre-alpha.com>
2025-01-10 13:04:06 -0500

    _

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+--no-sourcemap#!/bin/basg
 
 rm -f index.html main.bc.js
-cp ../sdui2/_build/default/bin/{index.html,main.bc.js} ./ 
+cp ../sdui4/_build/default/bin/{index.html,main.bc.js} ./ 
 git add -A
 git commit -m "_"
 git push
